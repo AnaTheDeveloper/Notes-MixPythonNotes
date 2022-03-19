21011f381d2e19512becf35db845e5f3fd47b2bd
Ana Edwards <edwards.ana81@gmail.com>
2022-03-19 16:55:45 +0000

    EDA

---
+++ src/PreProcessingDataStage/Exploratory Data Analysis (EDA)/EDANotes.py
@@ -0,0 +1,41 @@
+# The term Exploratory Data Analysis (EDA for short) refers to the process of discovering patterns and relationships
+# within a dataset. EDA is about making sense of the data at hand and getting to know it, often before modeling or
+# analysis. EDA is an important step prior to model building and can help you formulate further questions and areas
+# for investigation.
+
+# the main goals of EDA are generally:
+# Uncover the data structure and determine how it is coded.
+# Inspect and “get to know” the data by summarizing and visualizing it.
+# Detect outliers, missing data, and other anomalies and decide how/whether to address these issues.
+# Find new avenues for analysis and further research.
+# Prepare for model building or analysis, including the following:
+    # Check assumptions
+    # Select features
+    # Choose an appropriate method
+
+# EDA techniques:
+    # Data inspection
+    # Numerical summarization
+    # Data visualization
+
+# Inspect, Clean, and Validate a Dataset
+
+# Initial Data Inspection
+
+# Before analysis or cleaning, it is useful to print a few rows of data. This helps ensure that the data is properly
+# loaded. It also allows us to compare the observed data to the data dictionary and determine whether the
+# coding appears to match our expectations.
+
+# Data Information
+
+# Once we’ve taken a first look at some data, a common next step is to address questions such as:
+# How many (non-null) observations do we have?
+# How many unique columns/features do we have?
+# Which columns (if any) contain missing data?
+# What is the data type of each column?
+
+# Inspecting Missing Data
+
+# Find out WHY the data is missing, which can help us make decisions about whether to get rid of the
+# rows altogether or impute the missing values somehow.
+
